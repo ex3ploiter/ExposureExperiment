8c0cf83efb5d47c359177f372cb4018a504ee88e
Mohammad <mamad.jafari91@gmail.com>
2023-03-02 17:00:52 +0330

    handle normal dataset

--- datasets.py
+++ datasets.py
@@ -1,4 +1,14 @@
 
+from torchvision.datasets import CIFAR10, MNIST, SVHN, FashionMNIST
+import os
+import torch
+import numpy as np
+import torchvision.transforms as transforms
+from torch.utils.data import Dataset
+from PIL import Image
+from glob import glob
+
+
 class MyDataset_Binary(torch.utils.data.Dataset):
   'Characterizes a dataset for PyTorch'
   def __init__(self, x, labels,transform):
@@ -18,94 +28,149 @@ class MyDataset_Binary(torch.utils.data.Dataset):
         y = self.labels[index]
        
         return x, y
+  
+tansform_224 = transforms.Compose([
+                                      transforms.CenterCrop(224),
+                                      transforms.ToTensor()
+                                      ])
 
-def get_loaders(dataset, exposure_dataset='cifar10' train_batch_size=40, test_batch_size=300):
+tansform_224_gray = transforms.Compose([
+                                 transforms.CenterCrop(224),
+                                 transforms.Grayscale(num_output_channels=3),
+                                 transforms.ToTensor()
+                                ])
 
-    trans_to_32 = transforms.Compose([  transforms.Resize((32,32))])
+tansform_32 = transforms.Compose([
+                                      transforms.CenterCrop(32),
+                                      transforms.ToTensor()
+                                      ])
 
-    trans_to_28 = transforms.Compose([  transforms.Resize((28,28))])
+tansform_32_gray = transforms.Compose([
+                                 transforms.CenterCrop(32),
+                                 transforms.Grayscale(num_output_channels=3),
+                                 transforms.ToTensor()
+                                ])
+
+mvtec_labels = ['bottle', 'cable', 'capsule', 'carpet', 'grid', 'hazelnut', 'leather',
+                'metal_nut', 'pill', 'screw', 'tile', 'toothbrush', 'transistor',
+                'wood', 'zipper']
 
 
-    img_transform_32 = transforms.Compose([transforms.ToTensor(),transforms.Resize((32,32))])
+def get_normal_class(dataset='cifar', path='~/mydataset', normal_class_indx = 0, batch_size=8, img_size=32):
 
-    img_transform_simple = transforms.Compose([transforms.Resize((32,32)), transforms.Grayscale(3), transforms.ToTensor()])
+    assert img_size == 32 or img_size == 224
 
-    simple_train_ds_MNIST =  MNIST(root = './', train=True, download=True, transform=img_transform_simple)
-    cnt_normal = sum(np.asarray(simple_train_ds_MNIST.targets) == normal_class)
+    if dataset == 'cifar10':
+        return get_CIFAR10_normal(normal_class_indx, batch_size, path, img_size)
+    elif dataset == 'mnist':
+        return get_MNIST_normal(normal_class_indx, batch_size, path, img_size)
+    elif dataset == 'fashion':
+        return get_FASHION_MNIST_normal(normal_class_indx, batch_size, path, img_size)
+    elif dataset == 'svhn':
+        return get_SVHN_normal(normal_class_indx, batch_size, path, img_size)
+    elif dataset == 'mvtec':
+        return get_MVTEC(normal_class_indx, batch_size, path, img_size)
+    else:
+        raise Exception("Dataset is not supported yet. ")
 
-    idx = (simple_train_ds_MNIST.targets==normal_class) 
-    simple_train_ds_MNIST.targets = simple_train_ds_MNIST.targets[idx]
-    simple_train_ds_MNIST.data = simple_train_ds_MNIST.data[idx]
 
-    images_train = []
-    for x,_ in simple_train_ds_MNIST:
-        images_train.append(x)
-    MNIST_images_train_t = torch.stack(images_train)
+def get_CIFAR10_normal(normal_class_indx, batch_size, path, img_size):
+    tansform_dataset = tansform_224 if img_size==224 else tansform_32
 
-    if len(exposures) == 2:
-        MNIST_images_train_t = np.concatenate([MNIST_images_train_t.numpy(),MNIST_images_train_t.numpy()])
-        MNIST_images_train_t = torch.from_numpy(MNIST_images_train_t)
+    trainset = CIFAR10(root=path, train=True, download=True, transform=tansform_dataset)
+    trainset.data = trainset.data[np.array(trainset.targets) == normal_class_indx]
 
-    images_train_t = MNIST_images_train_t
-    ########
+    testset = CIFAR10(root=path, train=False, download=True, transform=tansform_dataset)
+    testset.targets  = [int(t!=normal_class_indx) for t in testset.targets]
 
-    if "gluide" in exposures:
-        loaded = np.load(fakes_path)
-        generated_fakes = trans_to_32(trans_to_28(torch.from_numpy(loaded)))
-        scaled = ((generated_fakes + 1)*127.5).round().clamp(0,255).to(torch.uint8).cpu()
-        generated_fakes = scaled/255
+    return trainset.data, testset
 
-        gray_scale_transformation_tr = torchvision.transforms.Grayscale(num_output_channels=3)
-        generated_fakes = gray_scale_transformation_tr(generated_fakes)[:cnt_normal]
 
-    if "cifar10" in exposures:
-        Cifar10_testset = CIFAR10('./Dataset/CIFAR10/train', train=True, download=True, transform=img_transform_32)
 
-        testset_cifar10=[]
-        for x,_ in Cifar10_testset:
-            testset_cifar10.append(x)
-        Cifar10_test_tensor = torch.stack(testset_cifar10[:cnt_normal])
+def get_MNIST_normal(normal_class_indx, batch_size, path, img_size):
+    tansform_dataset = tansform_224_gray if img_size==224 else tansform_32_gray
 
-    if "gluide" in exposures and "cifar10" in exposures:
-        Fakes = torch.from_numpy(np.concatenate([generated_fakes.numpy() , Cifar10_test_tensor.numpy()]))
-    elif "gluide" in exposures:
-        Fakes = generated_fakes
-    elif "cifar10" in exposures:
-        Fakes = Cifar10_test_tensor
+    trainset = MNIST(root=path, train=True, download=True, transform=tansform_dataset)
+    trainset.data = trainset.data[np.array(trainset.targets) == normal_class_indx]
 
-    simple_test_ds =  MNIST(root = './', train=False, download=True, transform=img_transform_simple)
-    test_labels=[]
-    images_test=[]
+    testset = MNIST(root=path, train=False, download=True, transform=tansform_dataset)
+    testset.targets  = [int(t!=normal_class_indx) for t in testset.targets]
 
-    for x,y in simple_test_ds:
-        images_test.append(x)
-        if y==normal_class:
-            test_labels.append(0)
+    return trainset.data, testset
+
+
+def get_FASHION_MNIST_normal(normal_class_indx, batch_size, path, img_size):
+    tansform_dataset = tansform_224_gray if img_size==224 else tansform_32_gray
+
+    trainset = FashionMNIST(root=path, train=True, download=True)
+    trainset.data = trainset.data[np.array(trainset.targets) == normal_class_indx]
+
+    testset = FashionMNIST(root=path, train=False, download=True)
+    testset.targets  = [int(t!=normal_class_indx) for t in testset.targets]
+
+    return trainset.data, testset
+
+def get_SVHN_normal(normal_class_indx, batch_size, path, img_size):
+    tansform_dataset = tansform_224 if img_size==224 else tansform_32
+
+    trainset = SVHN(root=path, split='train', download=True,)
+    trainset.data = trainset.data[np.array(trainset.labels) == normal_class_indx]
+
+    testset = SVHN(root=path, split='test', download=True)
+    testset.labels  = [int(t!=normal_class_indx) for t in testset.labels]
+
+    return trainset.data, testset
+
+
+class MVTecDataset(Dataset):
+    def __init__(self, root, category, transform=None, target_transform=None, train=True, normal=True):
+        self.transform = transform
+        if train:
+            self.image_files = glob(
+                os.path.join(root, category, "train", "good", "*.png")
+            )
         else:
-            test_labels.append(1)
-    images_test_t = torch.stack(images_test)
+          image_files = glob(os.path.join(root, category, "test", "*", "*.png"))
+          normal_image_files = glob(os.path.join(root, category, "test", "good", "*.png"))
+          anomaly_image_files = list(set(image_files) - set(normal_image_files))
+          self.image_files = image_files
 
-    print(images_train_t.shape[0])
+        self.image_files.sort(key=lambda y: y.lower())
+        self.train = train
 
-    Binary_Trainig_ = torch.cat((images_train_t , Fakes[:images_train_t.shape[0]]))
-    normlabel_ = [0] * images_train_t.shape[0]
-    abnormal_label_ = [1] * images_train_t.shape[0]
-    labels_train = normlabel_ + abnormal_label_
-    mir_data_set_Binary = MyDataset_Binary(Binary_Trainig_, labels_train, trans_to_32)
-    train_loader = DataLoader(mir_data_set_Binary, train_batch_size=40, shuffle=True)
+    def __getitem__(self, index):
+        image_file = self.image_files[index]
+        image = Image.open(image_file)
+        image = image.convert('RGB')
+        if self.transform is not None:
+            image = self.transform(image)
+
+        if os.path.dirname(image_file).endswith("good"):
+            target = 0
+        else:
+            target = 1
+
+        return image, target
+
+    def __len__(self):
+        return len(self.image_files)
 
 
+def get_MVTEC(normal_class_indx, batch_size, path):
+    normal_class = mvtec_labels[normal_class_indx]
 
-    mir_data_set_test = MyDataset_Binary(images_test_t, test_labels, trans_to_32)
-    test_loader = DataLoader(mir_data_set_test, test_batch_size=300, shuffle=False)
-    ######
+    trainset = MVTecDataset(path, normal_class, train=True)
+    testset = MVTecDataset(path, normal_class, train=False)
 
-    show_samples(Fakes[:64])
-    show_samples(images_train_t[:64])
+    return trainset[:, 0], testset
 
 
-    print("This is len of Train normal :",len(train_loader.dataset))
-    print("This is len of test set:",len(test_loader.dataset))
-
-    return train_loader, test_loader
-
+def download_and_extract_mvtec(path):
+    import os
+    import wget
+    import tarfile
+    so.extractall(path=os.environ['BACKUP_DIR'])
+    url = 'https://www.mydrive.ch/shares/38536/3830184030e49fe74747669442f0f282/download/420938113-1629952094/mvtec_anomaly_detection.tar.xz'
+    filename = wget.download(url, out=path)
+    with tarfile.open(os.path.join(path, filename)) as so:
+        so.extractall(path=os.path.join(path, "mvtec_anomaly_detection"))
