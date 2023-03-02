d78fe2683dcdfff4fa94060bcaae189247cdec7e
Mohammad <mamad.jafari91@gmail.com>
2023-03-02 17:09:41 +0330

    add constants.py

---
+++ constants.py
@@ -0,0 +1,7 @@
+# Change this path based on your system paths
+CIFAR10_PATH = '~/CIFAR10/'
+CIFAR100_PATH = '~/CIFAR100/'
+MNIST_PATH = '~/MNIST/'
+FMNIST_PATH = '~/FMNIST/'
+SVHN_PATH = '~/SVHN/'
+MVTEC_PATH = '~/MVTEC/'
