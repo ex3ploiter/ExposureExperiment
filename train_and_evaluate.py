d4d68ddcf3a2115af955cc83e1c668634ed12810
Mohammad <mamad.jafari91@gmail.com>
2023-03-03 15:02:44 +0330

    Complete Training Procedure

--- train_and_evaluate.py
+++ train_and_evaluate.py
@@ -9,9 +9,63 @@ from tqdm import tqdm
 from torchattacks import FGSM, PGD
 from models import Net
 from constants import PGD_CONSTANT
+from sklearn.metrics import roc_auc_score, accuracy_score
 
-def train_one_epoch(epoch, max_epochs, model, optimizer, criterion, trainloader, train_attack, lr=0.1):    
+
+
+def run(model, checkpoint, train_attack, test_attacks, trainloader, testloader, writer, test_step, max_epochs):
+
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.1, momentum=0.9, weight_decay=5e-4)
+    criterion = nn.CrossEntropyLoss()
+
+    init_epoch = 0
+
+    for epoch in range(init_epoch, max_epochs+1):
+
+        torch.cuda.empty_cache()
+
+        if epoch % test_step == 0 :
+                
+                test_auc = {}
+                test_accuracy = {}
+
+                clean_auc, clean_accuracy  = auc_softmax(model=model, epoch=epoch, test_loader=testloader, device=device)
+                test_auc['Clean'], test_accuracy['Clean'] = clean_auc, clean_accuracy
+
+                for attack_name, attack in test_attacks.items():
+                    adv_auc, adv_accuracy = auc_softmax_adversarial(model=model, epoch=epoch, test_loader=testloader, test_attack=attack, device=device)
+                    test_auc[attack_name], test_accuracy[attack_name] = adv_auc, adv_accuracy
+
+                writer.add_scalars('AUC-Test', test_auc, epoch)
+                writer.add_scalars('Accuracy-Test', test_accuracy, epoch)
+                writer.close()
+
+        torch.cuda.empty_cache()
+
+        train_auc, train_accuracy, train_loss = train_one_epoch(epoch=epoch,\
+                                                                max_epochs=max_epochs, \
+                                                                model=model,\
+                                                                optimizer=optimizer,
+                                                                criterion=criterion,\
+                                                                trainloader=trainloader,\
+                                                                train_attack=train_attack,\
+                                                                lr=0.1)
+        
+
+        writer.add_scalar('AUC-Train', train_auc, epoch)
+        writer.add_scalar('Accuracy-Train', train_accuracy, epoch)
+        writer.add_scalar('Accuracy-Loss', train_loss, epoch)
+        writer.close()
+
+
+
+
+def train_one_epoch(epoch, max_epochs, model, optimizer, criterion, trainloader, train_attack, lr=0.1): 
+
+    soft = torch.nn.Softmax(dim=1)
+
     preds = []
+    anomaly_scores = []
     true_labels = []
     running_loss = 0
     accuracy = 0
@@ -51,6 +105,9 @@ def train_one_epoch(epoch, max_epochs, model, optimizer, criterion, trainloader,
             correct = (torch.tensor(preds) == torch.tensor(true_labels)).sum().item()
             accuracy = correct / len(preds)
 
+            probs = soft(output).squeeze()
+            anomaly_scores += probs[:, 1].detach().cpu().numpy().tolist()
+
             running_loss += loss.item() * data.size(0)
 
             tepoch.set_postfix(loss=running_loss / len(preds), accuracy=100. * accuracy)
@@ -66,7 +123,10 @@ def train_one_epoch(epoch, max_epochs, model, optimizer, criterion, trainloader,
     # # writer.add_scalars
     # results["Train Accuracy"].append(100. * accuracy)
     # results["Loss"].append(running_loss / len(preds))
-    return accuracy, running_loss / len(preds)
+
+    return  roc_auc_score(true_labels, anomaly_scores) , \
+            accuracy_score(true_labels, preds, normalize=True), \
+            running_loss / len(preds)
 
 
 args = argsparser.parse_args()
